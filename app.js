0523dccdfef23169a0e80a55ccd25c678b7bd3af
Shadman Sakib <shadman.skb@gmail.com>
2017-01-19 15:34:42 +0600

    Add MongoLab

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index")
 
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
+// mongoose.connect("mongodb://localhost/yelp_camp_v10");
+
+mongoose.connect("mongodb://shadman:access2210@ds117819.mlab.com:17819/shadman_yelpcamp")
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
